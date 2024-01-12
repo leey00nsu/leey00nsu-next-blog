249cd34ea1c4261259054c54561bae603622b068
leey00nsu <dbstndla1212@naver.com>
2024-01-12 20:43:51 +0900

    refactor. 페이지네이션 쿼리 스트링 방식으로 변경

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,13 +3,15 @@
 import { Pagination } from "@nextui-org/react";
 import { allPosts } from "contentlayer/generated";
 import { compareDesc } from "date-fns";
-import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import PostCard from "../components/PostCard";
 
 const PAGE_SIZE = 5;
 
 export default function Home() {
-  const [currentPage, setCurrentPage] = useState(1);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get("page")) || 1;
 
   const posts = allPosts.sort((a, b) =>
     compareDesc(new Date(a.date), new Date(b.date))
@@ -20,11 +22,14 @@ export default function Home() {
     (currentPage - 1) * PAGE_SIZE + PAGE_SIZE
   );
 
+  const changePageHandler = (page: number) => {
+    router.push("/blog?page=" + page);
+  };
+
   const totalPageLength = Math.ceil(posts.length / PAGE_SIZE);
 
   return (
     <div className="mx-auto max-w-xl p-8 flex flex-col gap-4 ">
-      {/* <PostTags postTags={parseTag(allPosts)} /> */}
       <h2>{posts.length}개의 글이 있습니다.</h2>
       {paginatedPosts.map((post, idx) => (
         <PostCard key={idx} {...post} />
@@ -37,7 +42,7 @@ export default function Home() {
         showControls
         total={totalPageLength}
         page={currentPage}
-        onChange={setCurrentPage}
+        onChange={changePageHandler}
         initialPage={1}
       />
     </div>
