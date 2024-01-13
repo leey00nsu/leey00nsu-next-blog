ca946e7f729baeb2932217e63206bd18e3eb362c
leey00nsu <dbstndla1212@naver.com>
2024-01-13 16:01:12 +0900

    chore. *.config.* 에 대해 eslint를 작동하지 않도록 함

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,49 +1,50 @@
-// contentlayer.config.ts
-import { defineDocumentType, makeSource } from "contentlayer/source-files";
-import highlight from "rehype-highlight";
-import rehypePrettyCode from "rehype-pretty-code";
-import remarkGfm from "remark-gfm";
+// @ts-nocheck
+
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+import highlight from 'rehype-highlight';
+import rehypePrettyCode from 'rehype-pretty-code';
+import rehypeSlug from 'rehype-slug';
+import remarkGfm from 'remark-gfm';
 
 export const Post = defineDocumentType(() => ({
-  name: "Post",
-  contentType: "mdx",
+  name: 'Post',
+  contentType: 'mdx',
   filePathPattern: `**/*.mdx`,
   fields: {
-    title: { type: "string", required: true },
-    date: { type: "date", required: true },
-    slug: { type: "string", required: true },
+    title: { type: 'string', required: true },
+    date: { type: 'date', required: true },
+    slug: { type: 'string', required: true },
     tags: {
-      type: "list",
+      type: 'list',
       of: {
-        type: "string",
+        type: 'string',
       },
       required: true,
     },
-    description: { type: "string", required: true },
+    description: { type: 'string', required: true },
   },
   computedFields: {
     url: {
-      type: "string",
+      type: 'string',
       resolve: (post) => `${post._raw.sourceFileDir}`,
     },
   },
 }));
 
 export default makeSource({
-  contentDirPath: "public/posts",
+  contentDirPath: 'public/posts',
   documentTypes: [Post],
   mdx: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [
       [
-        // @ts-ignore
         rehypePrettyCode,
         {
-          theme: "slack-dark",
+          theme: 'slack-dark',
         },
       ],
-      // @ts-ignore
       highlight,
+      rehypeSlug,
     ],
   },
 });
