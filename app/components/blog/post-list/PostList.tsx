d31365df227de71e269070508691e1de207feaaf
leey00nsu <dbstndla1212@naver.com>
2024-01-19 19:12:24 +0900

    refactor. PostList params 예외처리

--- app/components/blog/post-list/PostList.tsx
+++ app/components/blog/post-list/PostList.tsx
@@ -1,17 +1,18 @@
-'use client';
-
 import { allPosts } from '@/.contentlayer/generated';
 import { compareDesc } from 'date-fns';
-import { useSearchParams } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 import PostItem from './PostItem';
-import Pagination from './PostPagination';
+import PostPagination from './PostPagination';
 
 const PAGE_SIZE = 6;
 
-const PostList = () => {
-  const searchParams = useSearchParams();
-  const currentPage = Number(searchParams.get('page')) || 1;
+const PostList = ({
+  searchParams,
+}: {
+  searchParams: Record<string, string | string[] | undefined>;
+}) => {
+  const currentPage = Number(searchParams?.page) || 1;
 
   const posts = allPosts.sort((a, b) =>
     compareDesc(new Date(a.date), new Date(b.date)),
@@ -24,15 +25,20 @@ const PostList = () => {
 
   const totalPageLength = Math.ceil(posts.length / PAGE_SIZE);
 
+  if (currentPage > totalPageLength) redirect(`/blog?page=${totalPageLength}`);
+
   return (
     <article className="mx-auto flex min-h-[calc(100svh-128px)] max-w-5xl flex-col justify-center gap-8 p-8">
       <h2>{posts.length}개의 글이 있습니다.</h2>
-      <section className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+      <section className="grid grow grid-cols-1 gap-4 sm:grid-cols-2">
         {paginatedPosts.map((post) => (
           <PostItem key={post.slug} {...post} />
         ))}
       </section>
-      <Pagination totalPageLength={totalPageLength} currentPage={currentPage} />
+      <PostPagination
+        totalPageLength={totalPageLength}
+        currentPage={currentPage}
+      />
     </article>
   );
 };
