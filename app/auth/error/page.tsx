56a3797a772d4ce91c57f23625223a0de85e74e2
leey00nsu <dbstndla1212@naver.com>
2024-01-31 21:23:33 +0900

    style. 의도하지않은 css 수정 PostList 그리드 크기, 에러페이지 prose, prose blockquotes

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -5,7 +5,7 @@ import LogoutButton from '@/app/components/auth/LogoutButton';
 const Error = async () => {
   return (
     <main className="min-w-screen flex min-h-[calc(100svh-128px)] flex-col items-center justify-center">
-      <div className="prose prose-slate flex flex-col items-center justify-center gap-4 dark:prose-invert">
+      <div className="prose-custom prose prose-slate flex flex-col items-center justify-center gap-4 dark:prose-invert">
         <FaRegSadCry className="h-20 w-20" />
         <h1>관리자만 접근 가능한 페이지입니다.</h1>
         <LogoutButton />
--- app/components/blog/post-list/PostList.tsx
+++ app/components/blog/post-list/PostList.tsx
@@ -30,7 +30,7 @@ const PostList = ({
   return (
     <main className="mx-auto flex min-h-[calc(100svh-128px)] max-w-5xl flex-col justify-center gap-8 p-8">
       <h2>{posts.length}개의 글이 있습니다.</h2>
-      <section className="grid grow grid-cols-1 gap-4 sm:grid-cols-2">
+      <section className="grid grow grid-cols-1 grid-rows-3 gap-4 sm:grid-cols-2">
         {paginatedPosts.map((post) => (
           <PostItem key={post.slug} {...post} />
         ))}
