fc9a33dda2d9b3f3dfbb70c5b0f764aaca73b414
leey00nsu <dbstndla1212@naver.com>
2024-02-02 11:59:02 +0900

    refactor. getPostViewCount 코드 분리 및 try catch 추가

--- app/actions/getPostViewCount.ts
+++ app/actions/getPostViewCount.ts
@@ -4,43 +4,77 @@ import { cookies } from 'next/headers';
 
 import supabase from '../libs/supabase';
 
-const getPostViewCount = async (slug: string) => {
-  const cookieStore = cookies();
-  let currentViewCount: number;
-
-  const { data: postData } = await supabase
+const getPost = async (slug: string) => {
+  const { data: postData, error } = await supabase
     .from('posts')
     .select('*')
     .eq('slug', slug)
     .single();
 
-  if (postData) {
-    currentViewCount = postData.view;
-  } else {
-    const { data: newPostData } = await supabase
-      .from('posts')
-      .insert({ slug })
-      .select()
-      .single();
-
-    currentViewCount = newPostData!.view;
+  if (error) {
+    throw new Error(error.message);
   }
 
-  const postViewed = cookieStore.get('postViewed');
-  const postViewedArray = postViewed?.value.split(',') ?? [];
+  return postData;
+};
 
-  if (!postViewedArray.includes(slug)) {
-    const { data: updatedData } = await supabase
-      .from('posts')
-      .update({ view: currentViewCount + 1 })
-      .eq('slug', slug)
-      .select()
-      .single();
+const createPost = async (slug: string) => {
+  const { data: newPostData, error } = await supabase
+    .from('posts')
+    .insert({ slug })
+    .select()
+    .single();
 
-    currentViewCount = updatedData!.view;
+  if (error) {
+    throw new Error(error.message);
   }
 
-  return currentViewCount;
+  return newPostData;
+};
+
+const updatePostViewCount = async (slug: string, view: number) => {
+  const { data: updatedData, error } = await supabase
+    .from('posts')
+    .update({ view })
+    .eq('slug', slug)
+    .select()
+    .single();
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return updatedData;
+};
+
+const getPostViewCount = async (slug: string) => {
+  try {
+    const cookieStore = cookies();
+    let currentViewCount: number;
+
+    const postData = await getPost(slug);
+
+    if (postData) {
+      currentViewCount = postData.view;
+    } else {
+      const newPostData = await createPost(slug);
+
+      currentViewCount = newPostData.view;
+    }
+
+    const postViewed = cookieStore.get('postViewed');
+    const postViewedArray = postViewed?.value.split(',') ?? [];
+
+    if (!postViewedArray.includes(slug)) {
+      const updatedPost = await updatePostViewCount(slug, currentViewCount + 1);
+
+      currentViewCount = updatedPost.view;
+    }
+
+    return currentViewCount;
+  } catch (error: any) {
+    return 0;
+  }
 };
 
 export default getPostViewCount;
