831eb02a601026075db3b5681b2fead5e6bdf528
leey00nsu <dbstndla1212@naver.com>
2024-02-07 22:00:12 +0900

    chore. vercel 파일 경로 수정

--- src/actions/studio/getImageFileNames.ts
+++ src/actions/studio/getImageFileNames.ts
@@ -1,16 +1,29 @@
 'use server';
 
 import blogConfig from '@/blog.config';
-import fs from 'fs';
-import path from 'path';
+import { Octokit } from '@octokit/rest';
 
 const getImageFileNames = async (slug: string) => {
   const postPath = `${blogConfig.postPath}/${slug}`;
-  const dir = path.join(process.cwd(), postPath);
 
-  const imageFileNames = fs
-    .readdirSync(dir)
-    .filter((file) => file !== `${slug}.mdx`);
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_API_KEY,
+  });
+
+  const content = await octokit.rest.repos.getContent({
+    owner: blogConfig.owner,
+    repo: blogConfig.repo,
+    path: postPath,
+  });
+
+  const imageFileNames = content.data
+    // @ts-ignore
+    .map((file) => file.name)
+    .filter((file: string) => file !== `${slug}.mdx`);
+
+  // const imageFileNames = fs
+  //   .readdirSync(dir)
+  //   .filter((file) => file !== `${slug}.mdx`);
 
   return imageFileNames;
 };
