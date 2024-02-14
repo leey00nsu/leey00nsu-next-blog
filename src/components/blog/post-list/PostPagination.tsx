b9728691713a61495a719f3a294895bdaad9d78b
leey00nsu <dbstndla1212@naver.com>
2024-02-14 19:55:15 +0900

    fix. 페이지네이션이 태그를 반영하지 않는 버그 수정

--- src/components/blog/post-list/PostPagination.tsx
+++ src/components/blog/post-list/PostPagination.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import { Pagination } from '@nextui-org/react';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+import arrayToQueryString from '@/src/libs/arrayToQueryString';
 
 interface PostPaginationProps {
   totalPageLength: number;
@@ -13,9 +15,19 @@ const PostPagination = ({
   currentPage,
 }: PostPaginationProps) => {
   const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   const changePageHandler = (page: number) => {
-    router.push(`/blog?page=${page}`);
+    const tags = searchParams.getAll('tags');
+
+    if (tags.length === 0) {
+      router.push(`${pathname}?page=${page}`);
+    }
+    if (tags.length > 0) {
+      const newQueryString = arrayToQueryString('tags', tags);
+      router.push(`${pathname}?page=${page}&${newQueryString}`);
+    }
   };
 
   return (
