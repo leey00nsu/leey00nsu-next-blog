6b23e5477cc34e2d02dc555a97ccc0068928b732
leey00nsu <dbstndla1212@naver.com>
2024-05-18 15:25:15 +0900

    style. 폰트 파일 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,45 +8,9 @@ import './globals.css';
 import Providers from './providers';
 
 const pretendard = localFont({
-  src: [
-    {
-      path: 'fonts/Pretendard-Thin.subset.woff2',
-      weight: '100',
-    },
-    {
-      path: 'fonts/Pretendard-ExtraLight.subset.woff2',
-      weight: '200',
-    },
-    {
-      path: 'fonts/Pretendard-Light.subset.woff2',
-      weight: '300',
-    },
-    {
-      path: 'fonts/Pretendard-Regular.subset.woff2',
-      weight: '400',
-    },
-    {
-      path: 'fonts/Pretendard-Medium.subset.woff2',
-      weight: '500',
-    },
-    {
-      path: 'fonts/Pretendard-SemiBold.subset.woff2',
-      weight: '600',
-    },
-    {
-      path: 'fonts/Pretendard-Bold.subset.woff2',
-      weight: '700',
-    },
-    {
-      path: 'fonts/Pretendard-ExtraBold.subset.woff2',
-      weight: '800',
-    },
-    {
-      path: 'fonts/Pretendard-Black.subset.woff2',
-      weight: '900',
-    },
-  ],
+  src: './fonts/PretendardVariable.woff2',
   display: 'swap',
+  weight: '45 920',
   variable: '--font-pretendard',
 });
 
